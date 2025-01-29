2df60468cce86dc99ff20c66ce8db75ab8673295
Mizarc <contact@mizarc.dev>
2025-01-29 23:52:11 +1000

    Fix gradle build settings
    Dependencies updated to MC 1.21.3 as well as Kotlin to 2.0. Should be
    able to compile with shadowJar now.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,6 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-    kotlin("jvm") version "1.6.0"
-    id("com.github.johnrengelman.shadow") version "7.1.2"
+    kotlin("jvm") version "2.0.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "dev.mizarc"
@@ -31,28 +29,23 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
-    compileOnly("io.papermc.paper:paper-api:1.20.1-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.21.3-R0.1-SNAPSHOT")
     shadow("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("co.aikar:idb-core:1.0.0-SNAPSHOT")
     implementation("co.aikar:acf-paper:0.5.1-SNAPSHOT")
     implementation("com.zaxxer:HikariCP:5.0.1")
     implementation("com.github.stefvanschie.inventoryframework:IF:0.10.11")
     compileOnly("com.github.MilkBowl:VaultAPI:1.7")
-    compileOnly(files("libs/GSit.jar"))
 }
 
-tasks.shadowJar {
-    relocate("co.aikar.commands", "dev.mizarc.waystonewarps.acf")
-    relocate("co.aikar.locales", "dev.mizarc.waystonewarps.locales")
-    relocate("co.aikar.idb", "dev.mizarc.waystonewarps.idb")
-    relocate ("com.github.stefvanschie.inventoryframework", "dev.mizarc.waystonewarps.inventoryframework")
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 }
 
 tasks.test {
     useJUnitPlatform()
 }
 
-tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "17"
-    kotlinOptions.javaParameters = true
+tasks.shadowJar {
+    archiveClassifier = null
 }
