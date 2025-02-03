0146555cd2c1029519421acb10bcb4dda4ef3d9d
Mizarc <contact@mizarc.dev>
2025-02-03 10:50:56 +1000

    Add movement monitoring service
    This allows the teleport service to cancel pending teleports if the
    player moves.

--- src/main/kotlin/dev/mizarc/waystonewarps/application/services/MovementMonitorService.kt
+++ src/main/kotlin/dev/mizarc/waystonewarps/application/services/MovementMonitorService.kt
@@ -5,4 +5,5 @@ import java.util.UUID
 interface MovementMonitorService {
     fun monitorPlayerMovement(playerId: UUID, onMove: () -> Unit)
     fun stopMonitoringPlayer(playerId: UUID)
+    fun logPlayerMovement(playerId: UUID)
 }
